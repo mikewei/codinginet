c72d50a1f65076dca56ae9492e6c47127a5c7199
weibin1 [韦彬] <weibin1@xiaomi.com>
2015-12-09 23:53:31 +0800

    add full screen

--- web/app/controllers/articles_controller.rb
+++ web/app/controllers/articles_controller.rb
@@ -48,7 +48,11 @@ class ArticlesController < ApplicationController
 
       @article = article if article[:id] == params[:id]
     end 
-    render :nothing => true, :status => 404 if @article == nil
+    if @article == nil then
+      render :nothing => true, :status => 404
+    elsif params[:simple] then
+      render 'view_simple'
+    end
   end
 
   private
@@ -121,64 +125,6 @@ class ArticlesController < ApplicationController
     md = md.gsub(/{%.*?%}/, '')
     #Kramdown::Document.new(md).to_html
     Redcarpet::Markdown.new(MyRender, fenced_code_blocks: true).render(md)
-  end
-
-  def index_del
-    @articles = Array.new
-    @categories = Hash.new
-    Pathname.glob('../blogs/*') do |path|
-
-      fn = path.basename.to_s
-      fn =~ /^((\d{6})-([^.]+))\.(\w+)$/
-      article = { id: $1, month: $2, name: $3, type: $4 }
-
-      path.open("r") do |f|
-        htype, head, body = parse_file(f)
-        if htype == :yaml then
-          article[:attr] = parse_yaml(head)
-        elsif htype == :free then
-          article[:attr] = parse_free(head)
-        else
-          next
-        end
-
-        count = @categories[article[:attr]['category']]
-        if count == nil
-          @categories[article[:attr]['category']] = 1;
-        else
-          @categories[article[:attr]['category']] = count + 1
-        end
-
-        if article[:attr]['time']
-          article[:timestamp] = article[:attr]['time']
-        else
-          est_time = article[:month].gsub(/^(....)(..)$/, '\1-\2-01 18:00 +0800')
-          article[:timestamp] = Time.parse(est_time)
-        end
-
-        filter_out = false
-        if params.has_key?(:cat)
-          if article[:attr]['category'] != params[:cat]
-            filter_out = true
-          end
-          @tips = "Filtered by category: #{params[:cat]}"
-        end
-        if params.has_key?(:year)
-          if article[:timestamp].year.to_s != params[:year]
-            filter_out = true
-          end
-          @tips = "Filtered by year: #{params[:year]}"
-        end
-
-        if not filter_out
-          article[:text] = Hash.new
-          article[:text][:md] = body
-          article[:text][:html] = md_to_html(body)
-          @articles.push article
-        end
-      end
-      @articles.sort_by!{|a| a[:timestamp]}.reverse!
-    end 
   end
 
 end
